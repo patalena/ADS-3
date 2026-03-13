494c9bb4407e6fdb27de045309999a2a990a1e26
patalena <lenapat0212@gmail.com>
2026-03-13 20:12:03 +0300

    Патанина Елена 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,31 +2,31 @@
 #include <cstdint>
 #include "alg.h"
 
-uint64_t collatzMaxValue(uint64_t num){
+uint64_t collatzMaxValue(uint64_t num) {
     uint64_t res = num;
     uint64_t maxa = num;
-    while (res != 1){
-        if (res % 2 == 0){
+    while (res != 1) {
+        if (res % 2 == 0) {
             res = res / 2;
         }
         else{
             res = 3 * res + 1;
         }
-        if (res > maxa){
+        if (res > maxa) {
             maxa = res;
         }
     }
     return maxa;
 }
 
-unsigned int collatzLen(uint64_t num){
+unsigned int collatzLen(uint64_t num) {
     unsigned int dlina = 1;
     uint64_t tek = num;
-    while (tek != 1){
-        if (tek % 2 == 0){
+    while (tek != 1) {
+        if (tek % 2 == 0) {
             tek = tek / 2;
         }
-        else{
+        else {
             tek = 3 * tek + 1;
         }
         dlina++;
@@ -34,13 +34,13 @@ unsigned int collatzLen(uint64_t num){
     return dlina;
 }
 
-unsigned int seqCollatz(unsigned int* maxlen, uint64_t lbound, uint64_t rbound){
+unsigned int seqCollatz(unsigned int* maxlen, uint64_t lbound, uint64_t rbound) {
     unsigned int max_len = 0;
     unsigned int res = 0;
 
-    for (uint64_t ch = lbound; ch <= rbound; ++ch){
+    for (uint64_t ch = lbound; ch <= rbound; ++ch) {
         unsigned int len = collatzLen(ch);
-        if (len > max_len){
+        if (len > max_len) {
             max_len = len;
             res = ch;
         }
